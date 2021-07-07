e906b4299aa554f91271a96a98f15907acb18c38
Jie Meng <jmengxy@gmail.com>
2021-07-07 11:06:37 +0800

    update install_font_on_mac.py

--- Font/install_font_on_mac.py
+++ Font/install_font_on_mac.py
@@ -3,20 +3,21 @@ import os
 def findFilesRecurse(path, ls = None):
     if ls == None:
         ls = []
-        
+
     for p in os.listdir(path):
         p = os.path.join(path, p)
         if os.path.isdir(p):
             findFilesRecurse(p, ls)
         elif os.path.isfile(p):
             ls.append(p)
-            
+
     return ls
 
 # main
 for f in findFilesRecurse(os.getcwd() + "/Font/"):
-   if f.find("ttf") != -1: 
-	cmd = 'cp {0} /Library/Fonts'.format(f)
-	print(cmd)
-	os.system(cmd)
+    if f.lower().endswith('ttf') or f.lower().endswith('otf'):
+        cmd = 'cp {0} /Library/Fonts'.format(f)
+        print(cmd)
+        os.system(cmd)
+
 print('Done')
